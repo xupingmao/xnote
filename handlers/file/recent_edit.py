540a9ee4a96cefedefd837d6e2bed93dd3a7812c
xupingmao <xupingmao@gmail.com>
2017-03-15 23:45:18 +0800

    最近编辑的页码

--- handlers/file/recent_edit.py
+++ handlers/file/recent_edit.py
@@ -5,30 +5,41 @@ from FileDB import *
 import xauth
 import xutils
 
+def execute(sql):
+    return xutils.db_execute("db/data.db", sql)
+
 # 待优化
-def get_recent_modified(days):
+def get_recent_modified(days, page=1):
     user = xauth.get_current_user()
     if user is None:
         return []
     user_name = user["name"]
     if user_name == "admin":
-        sql = "select * from file where smtime > '%s' AND is_deleted != 1 order by smtime desc"\
-        % dateutil.before(days=int(days), format=True)
+        # sql = "select * from file where smtime > '%s' AND is_deleted != 1 order by smtime desc"\
+        # % dateutil.before(days=int(days), format=True)
+        sql = "select * from file where is_deleted != 1 order by smtime desc"
     else:
         sql = "select * from file where smtime > '%s' AND is_deleted != 1 AND (groups='%s' OR groups='*') order by smtime desc"\
         % (dateutil.before(days=int(days), format=True), user_name)
-    list = xutils.db_execute("db/data.db",sql)
+    sql += " LIMIT %s, 20" % page
+    list = execute(sql)
         
     return [FileDO.fromDict(item) for item in list]
 
+def get_pages():
+    return execute("SELECT COUNT(*) as count FROM file")[0].get("count")
 
 class handler(BaseHandler):
     """show recent modified files"""
     def default_request(self):
         s_days = self.get_argument("days", 30)
+        page = int(self.get_argument("page", 1))
+        page = max(1, page)
         days = int(s_days)
         files = get_recent_modified(days)
-        self.render("file-list.html", files = files[:20], key = "")
+        pages = get_pages()
+        self.render("file-list.html", files = files[:20], key = "", 
+            page = page, pages = pages, page_url="/file/recent_edit?page=")
 
     def json_request(self):
         s_days = self.get_argument("days", 7)
