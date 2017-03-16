cb1ac12962dadc306901ca477fb242c9b03f6df5
xupingmao <xupingmao@gmail.com>
2017-03-17 00:08:12 +0800

    Fix code/view_source行号

--- handlers/code/view_source.py
+++ handlers/code/view_source.py
@@ -9,7 +9,7 @@ class ViewSourceHandler(BaseHandler):
         path = self.get_argument("path", "")
         key  = self.get_argument("key", "")
         if path == "":
-            self.render("code/view-source.html", error = "path is empty")
+            self.render(error = "path is empty")
         else:
             try:
                 content = xutils.readfile(path)
@@ -17,8 +17,8 @@ class ViewSourceHandler(BaseHandler):
                     content = xutils.html_escape(content)
                     key     = xhtml_escape(key)
                     content = textutil.replace(content, key, htmlutil.span("?", "search-key"), ignore_case=True, use_template=True)
-                self.render("code/view-source.html", content = content, lines = content.count("\n"))
+                self.render(content = content, lines = content.count("\n")+1)
             except Exception as e:
-                self.render("code/view-source.html", error = e, lines = 0, content="")
+                self.render(error = e, lines = 0, content="")
 
 handler = ViewSourceHandler
