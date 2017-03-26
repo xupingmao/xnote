f83f012c17241c283935d7205f171a6c87907fc1
xupingmao <xupingmao@gmail.com>
2017-03-26 23:12:46 +0800

    Update view_source，服务器不处理关键字高亮，留给客户端处理(TODO)

--- handlers/code/view_source.py
+++ handlers/code/view_source.py
@@ -1,3 +1,8 @@
+# -*- coding:utf-8 -*-  
+# Created by xupingmao on 2016/??/??
+# 
+
+"""显示代码原文"""
 from handlers.base import *
 from tornado.escape import xhtml_escape
 
@@ -13,10 +18,11 @@ class ViewSourceHandler(BaseHandler):
         else:
             try:
                 content = xutils.readfile(path)
-                if key != "":
-                    content = xutils.html_escape(content)
-                    key     = xhtml_escape(key)
-                    content = textutil.replace(content, key, htmlutil.span("?", "search-key"), ignore_case=True, use_template=True)
+                # 使用JavaScript来处理搜索关键字高亮问题
+                # if key != "":
+                #     content = xutils.html_escape(content)
+                #     key     = xhtml_escape(key)
+                #     content = textutil.replace(content, key, htmlutil.span("?", "search-key"), ignore_case=True, use_template=True)
                 self.render(content = content, lines = content.count("\n")+1)
             except Exception as e:
                 self.render(error = e, lines = 0, content="")
