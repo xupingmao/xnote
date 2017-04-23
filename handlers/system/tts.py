be54a2c54b5f35e07ed01472a110f4f1b76f6962
xupingmao <xupingmao@gmail.com>
2017-04-23 14:40:54 +0800

    Add tts demo，使用Windows API，需要安装comtypes模块

---
+++ handlers/system/tts.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-  
+# Created by xupingmao on 2017/04/23
+# 
+
+"""Text To Speach"""
+
+import xutils
+from handlers.base import BaseHandler
+
+class handler(BaseHandler):
+
+    def default_request(self):
+        content = self.get_argument("content")
+        # print(content)
+        import comtypes.client as cc
+        # dynamic=True不生成静态的Python代码
+        voice = cc.CreateObject("SAPI.SpVoice", dynamic=True)
+        try:
+            voice.Speak(content)
+        finally:
+            voice.Release()
+            return "OK"
