e7d779394767d9b30910e05c1b28a87feacb6a57
xupingmao <xupingmao@gmail.com>
2017-04-23 17:28:27 +0800

    Update split_html，支持网站资源提取

--- handlers/tools/split_html.py
+++ handlers/tools/split_html.py
@@ -13,6 +13,10 @@ def get_addr(src, host):
         return "http:" + src
     return src
 
+def readhttp(address):
+    bytes = xutils.urlopen(address).read()
+    return xutils.decode_bytes(bytes)
+
 def get_addr_list(images, host=None):
     dest = set()
     for image in images:
@@ -46,6 +50,8 @@ def download_res_list(reslist, dirname):
         with open(path, "wb") as fp:
             fp.write(bytes)
 
+def isempty(str):
+    return str==None or len(str) == 0
 
 class handler:
 
@@ -56,12 +62,16 @@ class handler:
         args = web.input(file={}, download_res="off")
         file = args.file
         download_res = args.download_res == "on"
+        address = args.address
 
         filename = file.filename
 
-        html = ""
-        for chunk in file.file:
-            html += chunk.decode("utf-8")
+        if not isempty(address):
+            html = readhttp(address)
+        else:
+            html = ""
+            for chunk in file.file:
+                html += chunk.decode("utf-8")
         print("Read html, filename={}, length={}".format(filename, len(html)))
 
         soup = BeautifulSoup(html, "html.parser")
@@ -69,11 +79,12 @@ class handler:
         links  = soup.find_all("a")
         csses  = soup.find_all("link")
         scripts = soup.find_all("script")
-        texts = soup.find_all(["p", "span", "h1", "h2", "h3", "h4"])
+        # texts = soup.find_all(["p", "span", "div", "h1", "h2", "h3", "h4"])
+        texts = [soup.get_text()]
 
         images = get_addr_list(images)
         scripts = get_addr_list(scripts)
-        texts = get_text_list(texts)
+        # texts = get_text_list(texts)
 
         if download_res:
             download_res_list(images, filename)
