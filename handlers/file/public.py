63e55a4c2b08619b8e3f388e822527e4a3044c08
xupingmao <xupingmao@gmail.com>
2017-05-07 17:04:11 +0800

    Fix file/public

--- handlers/file/public.py
+++ handlers/file/public.py
@@ -6,23 +6,27 @@
 
 import xtemplate
 
+import handlers.base as base
 from xutils import Storage
 from . import dao
 
-def get_recent_modified(count):
+def get_recent_modified(limit, page=0):
     db = dao.FileDB()
     count = db.execute("SELECT COUNT(*) AS _cnt FROM file WHERE is_deleted != 1 AND groups = '*'")[0].get("_cnt")
-    all = db.execute("SELECT * FROM file WHERE is_deleted != 1 AND groups = '*' ORDER BY smtime DESC LIMIT %s" % count)
+    sql = "SELECT * FROM file WHERE is_deleted != 1 AND groups = '*'"
+    sql += " ORDER BY smtime DESC LIMIT %s, %s" % (page * limit, limit)
+    all = db.execute(sql)
     files = [dao.FileDO.fromDict(item) for item in all]
     return Storage(count = count, files = files)
 
 class handler:
 
     def GET(self):
-        recent = get_recent_modified(10)
+        page = base.get_argument("page", 1, type=int)
+        recent = get_recent_modified(10, page-1)
         return xtemplate.render("file-list.html", 
             count=recent.count, 
             files=recent.files, 
-            page=1,
+            page=page,
             page_url="/file/public?page=")
 
