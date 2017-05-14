de1b34c356d9f09e59d80dc3ea5123e0e7645ba4
xupingmao <xupingmao@gmail.com>
2017-05-14 21:26:54 +0800

    优化文件上传，使用HTML5文件API

--- handlers/file/upload_file.py
+++ handlers/file/upload_file.py
@@ -2,19 +2,31 @@
 import os
 import time
 
+import xutils
 from handlers.base import *
 
 def is_img(filename):
     name, ext = os.path.splitext(filename)
     return ext.lower() in (".gif", ".png", ".jpg", ".jpeg", ".bmp")
 
+def get_link(filename, webpath):
+    if is_img(filename):
+        return "![%s](%s)" % (filename, webpath)
+    return "[%s](%s)" % (filename, webpath)
+
+def print_env():
+    for key in web.ctx.env:
+        print(" - - %-20s = %s" % (key, web.ctx.env.get(key)))
+
 class handler(BaseHandler):
 
     def execute(self):
-        x = web.input(file = {})
+        file = xutils.get_argument("file", {})
+        type = xutils.get_argument("type", "normal")
         filepath = ""
         filename = ""
-        file = x.file
+
+        print_env()
 
         if not hasattr(file, "filename") or file.filename == "":
             self.render("file/upload_file.html", filepath = filepath, filename = filename)
@@ -25,6 +37,9 @@ class handler(BaseHandler):
             # fout.write(x.file.file.read())
             for chunk in file.file:
                 fout.write(chunk)
+
+        if type == "html5":
+            return dict(success=True, message="上传成功", link=get_link(filename, webpath))
         self.render("file/upload_file.html", 
             filepath = webpath, 
             filename = filename, 
