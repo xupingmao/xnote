a6a1dfe875895ed850da2c709d50daad76f85776
xupingmao <xupingmao@gmail.com>
2017-05-21 17:17:19 +0800

    标签功能

--- handlers/file/tag.py
+++ handlers/file/tag.py
@@ -3,6 +3,7 @@
 
 from . import dao
 import xutils
+import xtemplate
 
 class TagHandler:
     
@@ -35,14 +36,30 @@ class AddTagHandler:
             db.insert("file_tag", file_id=id, name=tag, groups=file.groups)
         return dict(code="", message="", data="OK")
 
-class TagListHandler:
+class TagNameHandler:
 
     def GET(self, tagname):
+        tagname = xutils.unquote(tagname)
         db = dao.get_file_db()
-        tag_list = db.select("file_tag", where="UPPER(name) = $name", vars=dict(name=tagname.upper()))
-        return dict(code="", message="", data=list(tag_list))
+        offset = xutils.get_argument("offset", 0, type=int)
+        limit  = xutils.get_argument("limit", 10, type=int)
+        # tag_list = db.select("file_tag", where="UPPER(name) = $name", vars=dict(name=tagname.upper()))
+        files = db.query("SELECT f.* FROM file f, file_tag ft ON ft.file_id = f.id WHERE UPPER(ft.name) = $name ORDER BY f.sctime DESC LIMIT $offset, $limit", 
+            vars=dict(name=tagname.upper(), offset=offset, limit=limit))
+        files = [dao.FileDO.fromDict(f) for f in files]
+        return xtemplate.render("file-list.html", files=files)
+        # return dict(code="", message="", data=list(tag_list))
+
+class TagListHandler:
+
+    def GET(self):
+        db = dao.get_file_db()
+        tag_list = db.query("SELECT name, COUNT(*) AS amount FROM file_tag GROUP BY name")
+        return xtemplate.render("file/taglist.html", tag_list = list(tag_list))
+        # return dict(code="", message="", data=tag_list)
 
 xurls = (r"/file/tag/(\d+)", TagHandler,
          r"/file/tag/add", AddTagHandler,
-         r"/file/taglist/(.*)", TagListHandler)
+         r"/file/tagname/(.*)", TagNameHandler,
+         r"/file/taglist", TagListHandler)
 
--- static/js/array.js
+++ static/js/array.js
@@ -1,4 +1,6 @@
-
+/**
+ * Array 兼容增强包
+ */
 Array.prototype.startsWith = Array.prototype.startsWith || function (dst) {
     var array = this;
     for (var key in array) {
@@ -13,5 +15,17 @@ Array.prototype.each = Array.prototype.each || function (callback) {
     for (var i = 0; i < self.length; i++) {
         var item = self[i];
         callback(i, item);
+    }
+}
+
+/**
+ * forEach遍历
+ * @param callback
+ */
+Array.prototype.forEach = Array.prototype.forEach || function (callback) {
+    var self = this;
+    for (var i = 0; i < self.length; i++) {
+        var item = self[i];
+        callback(item, i, self);
     }
 }
