96434f666b8b3e428ec5d9bde390a8cc275bde9c
xupingmao <xupingmao@gmail.com>
2017-06-23 23:11:16 +0800

    统计PV，UV

--- handlers/system/stats.py
+++ handlers/system/stats.py
@@ -4,15 +4,21 @@ import web
 import xtables
 import xutils
 
+def save_ip(real_ip):
+    if real_ip is not None:
+        db = xtables.get_record_table()
+        record = db.select_one(where=dict(type="ip", key=real_ip, cdate=xutils.format_date()))
+        if record is None:
+            db.insert(type="ip", key=real_ip, cdate=xutils.format_date(), 
+                ctime=xutils.format_datetime(), value="1")
+        else:
+            db.update(value=int(record.value)+1, where=dict(id=record.id))
+
 class handler:
 
     def GET(self):
-        real_ip = web.ctx.env.get("HTTP_X_REAL_IP")
-        if real_ip is not None:
-            db = xtables.get_record_table()
-            record = db.select_one(where=dict(type="ip", key=real_ip, cdate=xutils.format_date()))
-            if record is None:
-                db.insert(type="ip", key=real_ip, cdate=xutils.format_date(), 
-                    ctime=xutils.format_datetime(), value="1")
+        save_ip(web.ctx.env.get("HTTP_X_REAL_IP"))
+        save_ip(web.ctx.env.get("REMOTE_ADDR"))
+        
         web.header("Content-Type", "application/javascript")
         return "/** stats **/"
