fa984d4820e873d5f7a9d6db144a15bde8647d29
xupingmao <xupingmao@gmail.com>
2017-06-28 19:05:26 +0800

    优化stats.py

--- handlers/system/stats.py
+++ handlers/system/stats.py
@@ -1,11 +1,14 @@
 # encoding=utf-8
 # Created by xupingmao on 2017/06/23
+# Last Modified on 2017/06/28
 import web
 import xtables
 import xutils
 
 def save_ip(real_ip):
     if real_ip is not None:
+        # 处理X-Forwarded-For
+        real_ip = real_ip.split(",")[0]
         # 跳过内网
         # A类和C类
         if real_ip.startswith("10.") or real_ip.startswith("192.168"):
@@ -21,7 +24,12 @@ def save_ip(real_ip):
 class handler:
 
     def GET(self):
-        save_ip(web.ctx.env.get("HTTP_X_REAL_IP"))
+        # X-Forwarded-For是RFC 7239（Forwarded HTTP Extension）定义的扩展首部，大部分代理支持
+        # 格式如下：
+        # X-Forwarded-For: client, proxy1, proxy2
+        # 而X-Real-IP目前不属于任何标准
+        # See https://imququ.com/post/x-forwarded-for-header-in-http.html
+        save_ip(web.ctx.env.get("HTTP_X_FORWARDED_FOR"))
         save_ip(web.ctx.env.get("REMOTE_ADDR"))
         
         web.header("Content-Type", "application/javascript")
