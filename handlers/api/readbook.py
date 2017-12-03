38624a63405a521a7a2124e88c6cd83bc50b9f58
xupingmao <xupingmao@gmail.com>
2017-12-03 15:03:20 +0800

    优化阅读器缓存

--- handlers/api/readbook.py
+++ handlers/api/readbook.py
@@ -23,24 +23,34 @@ class handler:
 
     def seek_page(self, fp, bookmark, pagesize):
         # 缓存
+        mod = 20
         page = bookmark.get("page", 1)
         # cache = bookmark.get("cache", {})
         startpage = 0
 
+        if page < 0:
+            page = 0
         fp.seek(0)
-        # if len(cache) > 0:
-        #     startpage = sorted(cache.keys())[-1]
-        #     print("find startpage %s,%s" % (startpage, cache[startpage]))
-        #     fp.seek(cache[startpage])
+
+        startpage = int(page / mod) * mod
+        pos = bookmark.get("page_%s"%startpage)
+        if pos:
+            print("Hit cache, page=%s, position=%s" % (startpage, pos))
+            fp.seek(pos)
+        else:
+            startpage = 0
+
         for i in range(int(startpage), page-1):
             # cache[page] = fp.tell()
             fp.read(pagesize)
+        if page % mod == 0:
+            bookmark["page_%s" % page] = fp.tell()
         # bookmark["cache"] = cache
 
     @xauth.login_required("admin")
     def GET(self):
         path    = xutils.get_argument("path")
-        length  = xutils.get_argument("length", 200, type=int)
+        length  = 1000
         read    = xutils.get_argument("read", "false")
         direction = xutils.get_argument("direction", "forward")
         page    = xutils.get_argument("page", 1, type=int)
@@ -59,6 +69,8 @@ class handler:
             except:
                 pass
         page = bookmark.get("page", 1)
+        size = xutils.get_file_size(path, format=False)
+
         with open(path, encoding=encoding) as fp:
             text = "dummy"
             if direction == "backward":
@@ -67,10 +79,11 @@ class handler:
                 page = page + 1
             bookmark["page"] = page
             self.seek_page(fp, bookmark, length)
+            current = fp.tell()
             text = fp.read(length)
             if read == "true":
                 xutils.say(text)
             if direction in ("forward", "backward"):
                 xutils.savetofile(bookmarkpath, json.dumps(bookmark))
-        return dict(code="success", data=text, page=page)
+        return dict(code="success", data=text, page=page, current=current, size=size)
 
