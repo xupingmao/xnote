3c2ea1c3844b36e425df7ec04ccfdb8987bcdf1f
xupingmao <xupingmao@gmail.com>
2017-12-12 21:11:12 +0800

    Update 端口扫描工具

--- handlers/tools/port_scanner.py
+++ handlers/tools/port_scanner.py
@@ -1,5 +1,4 @@
 # encoding=utf-8
-
 import socket 
 import sys
 import threading
@@ -16,16 +15,18 @@ if not PY2:
     xrange = range
 
 
-class TextImage:
+class Console:
 
     def __init__(self):
         self.last = ''
 
-    def update(self, msg):
+    def update(self, msg, reset=False):
         with _lock:
             self.clear_message(len(self.last))
             self.print_message(msg)
             self.last = msg
+            if reset:
+                self.last = ''
 
     def print_message(self, msg):
         with _lock:
@@ -37,36 +38,34 @@ class TextImage:
         if length == 0:
             return
         back = '\b' * length
-        whitespace = '\b' * length
+        whitespace = ' ' * length
         sys.stdout.write(back)
         sys.stdout.write(whitespace)
         sys.stdout.write(back)
         sys.stdout.flush()
 
-def scan(addr, port, img): 
+def scan(addr, port): 
     s = socket.socket() 
     s.settimeout(0.01) 
     if s.connect_ex((addr, port)) == 0: 
-        img.update('%s is open\n' % port)
+        console.update('%s is open\n' % port, reset=True)
     s.close() 
 
 _lock = threading.RLock()
 _handled = 0
- 
+console = Console()
+
 def worker(addr, q, total):
     global _lock 
     global _handled
-    img = TextImage()
     while not q.empty(): 
         port = q.get() 
-        # print("process %s" % port)
         try: 
-            img.update("%d/%d" % (_handled, total))
-            scan(addr, port, img) 
+            console.update("progress: %d/%d" % (_handled, total))
+            scan(addr, port) 
         finally: 
             with _lock:
                 _handled += 1
-            # q.task_done() 
  
 if __name__ == '__main__': 
     addr  = sys.argv[1]
@@ -74,15 +73,13 @@ if __name__ == '__main__':
     end   = int(sys.argv[3])
     print("scan %s-%s" % (start, end))
     q = Queue() 
-    # map(q.put,xrange(1,65535)) 
     total = 0
     for port in range(start, end+1):
         q.put(port)
         total += 1
     # jobs = [multiprocessing.Process(target=worker, args=(q,total)) for i in range(100)] 
-    jobs = [threading.Thread(target=worker, args=(addr, q, total)) for i in range(10)]
+    jobs = [threading.Thread(target=worker, args=(addr, q, total)) for i in range(50)]
     for job in jobs:
         job.start()
-    # map(lambda x:x.start(),jobs)
 
 
