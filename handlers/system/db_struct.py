eeb6026b386c4be92eafc81d9b075fe7e7a2d53b
xupingmao <xupingmao@gmail.com>
2017-12-17 11:37:23 +0800

    Fix 登陆控件对齐问题，删除废弃代码

--- handlers/system/db_struct.py
+++
@@ -1,8 +0,0 @@
-from handlers.base import *
-from handlers.file import dao
-
-class handler(BaseHandler):
-
-    def execute(self):
-        table_struct = dao.get_table_struct("file")
-        self.render("system/db_struct.html", table_struct = table_struct)
