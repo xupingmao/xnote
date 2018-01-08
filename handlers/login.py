47dccac66436a200b991d384e2d238fd6e9d7b2e
xupingmao <xupingmao@gmail.com>
2018-01-08 19:36:29 +0800

    记录尝试登陆信息

--- handlers/login.py
+++ handlers/login.py
@@ -1,10 +1,16 @@
 # encoding=utf-8
 import web
 import hashlib
-
 import xutils
 import xauth
 import xtemplate
+import xtables
+
+def get_real_ip():
+    x_forwarded_for = web.ctx.env.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for != None:
+        return x_forwarded_for.split(",")[0]
+    return web.ctx.env.get("REMOTE_ADDR")
 
 class handler:
 
@@ -25,9 +31,15 @@ class handler:
                     raise web.seeother("/")
                 raise web.seeother(target)
             else:
-                error = "password error"
+                error = "user or password error"
         else:
-            error = "user not exists"
+            error = "user or password error"
+        if error != "":
+            db = xtables.get_record_table()
+            value = "%s-%s" % (get_real_ip(), pswd)
+            db.insert(type="login", key=name, value=value, 
+                ctime = xutils.format_datetime(), 
+                cdate = xutils.format_date())
         return xtemplate.render("login.html", 
             username=name, 
             password=pswd,
