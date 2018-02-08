65905f63175f63fb1791fedd3ddf7b5f70ef6b1e
徐平茂 <578749341@qq.com>
2018-02-08 22:58:32 +0800

    Fix find token

--- handlers/fs/find.py
+++ handlers/fs/find.py
@@ -2,6 +2,7 @@
 import os
 import glob
 import xutils
+import xauth
 import xtemplate
 
 class handler:
@@ -9,6 +10,7 @@ class handler:
     def GET(self):
         return self.POST()
 
+    @xauth.login_required("admin")
     def POST(self):
         path = xutils.get_argument("path")
         find_key = xutils.get_argument("find_key", "")
@@ -20,6 +22,7 @@ class handler:
         else:
             plist = xutils.search_path(path, find_key)
         return xtemplate.render("fs/fs.html", 
+            token = xauth.get_current_user().token,
             fspathlist = xutils.splitpath(path),
             filelist = [xutils.FileItem(p, path) for p in plist])
 
