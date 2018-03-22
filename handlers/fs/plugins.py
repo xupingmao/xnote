b44b019d77641d96ef758170d9db282cf88ae478
徐平茂 <578749341@qq.com>
2018-03-23 00:05:19 +0800

    过滤文件系统插件

--- handlers/fs/plugins.py
+++ handlers/fs/plugins.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # @author xupingmao <578749341@qq.com>
 # @since 2018/03/22 22:57:39
-# @modified 2018/03/22 23:16:21
+# @modified 2018/03/23 00:02:29
 import os
 import xconfig
 import xutils
@@ -10,7 +10,7 @@ import xtemplate
 class ListHandler:
     def GET(self):
         path = xutils.get_argument("path")
-        scripts = sorted(filter(lambda x: x.endswith(".py"), os.listdir(xconfig.SCRIPTS_DIR)))
+        scripts = sorted(filter(lambda x: x.endswith(".py") and x.startswith("fs"), os.listdir(xconfig.SCRIPTS_DIR)))
         return xtemplate.render("fs/plugins.html", path = path, scripts = scripts)
 
 
