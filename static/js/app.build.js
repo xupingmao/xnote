5bb3dbc56d0c0fcfe75c7e5369bb722327745e57
xupingmao <578749341@qq.com>
2020-05-31 19:17:03 +0800

    【优化】通过剪切板上传文件增加loading蒙版

--- static/js/app.build.js
+++ static/js/app.build.js
@@ -1054,13 +1054,12 @@ $(function () {
 });
 /** audio.js, part of xnote-ui 
  * @since 2020/01/05
- * @modified 2020/01/21 20:13:34
+ * @modified 2020/05/31 19:14:50
  **/
 
 $(function(e) {
 
-    $("body").on("click", ".x-audio",
-    function(e) {
+    $("body").on("click", ".x-audio", function(e) {
         var src = $(this).attr("data-src");
         layer.open({
             type: 2,
@@ -1076,6 +1075,14 @@ var BASE_URL = "/static/lib/webuploader";
 
 if (window.xnote == undefined) {
     window.xnote = {};
+}
+
+function createXnoteLoading() {
+    return loadingIndex = layer.load(2);
+}
+
+function closeXnoteLoading(index) {
+    layer.close(index);
 }
 
 /** 创建上传器 **/
@@ -1127,6 +1134,9 @@ window.xnote.createUploader = function(fileSelector, chunked) {
 // 把blob对象转换成文件上传到服务器
 window.xnote.uploadBlob = function(blob, prefix, successFn, errorFn) {
     var fd = new FormData();
+    // 加载页，用户阻止用户交互
+    var loadingIndex = createXnoteLoading();
+
     fd.append("file", blob);
     fd.append("prefix", prefix);
     fd.append("name", "auto");
@@ -1134,6 +1144,7 @@ window.xnote.uploadBlob = function(blob, prefix, successFn, errorFn) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/fs_upload');
     xhr.onload = function() {
+        closeXnoteLoading(loadingIndex);
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
                 var data = JSON.parse(xhr.responseText);
@@ -1147,8 +1158,10 @@ window.xnote.uploadBlob = function(blob, prefix, successFn, errorFn) {
             }
         };
     };
+
     xhr.onerror = function(e) {
         console.log(xhr.statusText);
+        closeXnoteLoading(loadingIndex);
     }
     xhr.send(fd);
 };
@@ -1162,43 +1175,39 @@ window.xnote.requestUpload = function(fileSelector, chunked, successFn, errorFn)
     var uploader = window.xnote.createUploader(fileSelector, chunked);
 
     // 当有文件添加进来的时候
-    uploader.on('fileQueued',
-    function(file) {
+    uploader.on('fileQueued', function(file) {
         // 添加文件
         console.log("file = " + file);
     });
 
     // 文件上传过程中创建进度条实时显示。
-    uploader.on('uploadProgress',
-    function(file, percentage) {
+    uploader.on('uploadProgress', function(file, percentage) {
         // 进度条
     });
 
-    uploader.on('uploadBeforeSend',
-    function(object, data, headers) {
+    uploader.on('uploadBeforeSend', function(object, data, headers) {
         data.dirname = "auto";
     })
 
     // 文件上传成功，给item添加成功class, 用样式标记上传成功。
-    uploader.on('uploadSuccess',
-    function(file, resp) {
+    uploader.on('uploadSuccess', function(file, resp) {
         console.log("uploadSuccess", file, resp);
 
-        layer.close(loadingIndex);
-
+        // 关闭加载页
+        closeXnoteLoading(loadingIndex);
+        // 回调成功函数
         successFn(resp);
     });
 
     // 文件上传失败，显示上传出错。
-    uploader.on('uploadError',
-    function(file) {
+    uploader.on('uploadError', function(file) {
         layer.alert('上传失败');
-        layer.close(loadingIndex);
+        // 关闭加载页
+        closeXnoteLoading(loadingIndex);
     });
 
     // 完成上传完了，成功或者失败，先删除进度条。
-    uploader.on('uploadComplete',
-    function(file) {
+    uploader.on('uploadComplete', function(file) {
         console.log("uploadComplete", typeof(file), file);
     });
 
@@ -1206,18 +1215,49 @@ window.xnote.requestUpload = function(fileSelector, chunked, successFn, errorFn)
     $(fileSelector).click();
 
     // 选择文件完毕
-    $(fileSelector).on("change",
-    function(event) {
+    $(fileSelector).on("change", function(event) {
         console.log(event);
         var fileList = event.target.files; //获取文件对象 
         if (fileList && fileList.length > 0) {
             uploader.addFile(fileList);
-            loadingIndex = layer.load(2);
+            // 创建加载页，阻止用户操作
+            loadingIndex = createXnoteLoading();
         }
         // 清空文件列表，不然下次上传会重复
         event.target.files = [];
     });
 };
+
+// 通过剪切板请求上传
+// @param {event} e 粘贴事件
+// @param {string} filePrefix 保存的文件名前缀
+// @param {function} successFn 成功的回调函数
+// @param {function} errorFn 失败的回调函数
+window.xnote.requestUploadByClip = function (e, filePrefix, successFn, errorFn) {
+    console.log(e);
+    var clipboardData = e.clipboardData || e.originalEvent && e.originalEvent.clipboardData || {};
+
+    // console.log(clipboardData);
+    if (clipboardData.items) {
+        items = clipboardData.items;
+        for (var index = 0; index < items.length; index++) {
+            var item  = items[index];
+            var value = item.value;
+            // console.log(item, value);
+            if (/image/i.test(item.type)) {
+                console.log(item);
+                var blob = item.getAsFile();
+                xnote.uploadBlob(blob, filePrefix, function (resp) {
+                    successFn(resp);
+                }, function (resp) {
+                    if (errorFn) {
+                        errorFn(resp);
+                    }
+                });
+            }
+        }
+    }
+}
 
 /** x-upload.js end **/
 if (window.xnote == undefined) {
--- static/js/xnote-ui/x-audio.js
+++ static/js/xnote-ui/x-audio.js
@@ -1,12 +1,11 @@
 /** audio.js, part of xnote-ui 
  * @since 2020/01/05
- * @modified 2020/01/21 20:13:34
+ * @modified 2020/05/31 19:14:50
  **/
 
 $(function(e) {
 
-    $("body").on("click", ".x-audio",
-    function(e) {
+    $("body").on("click", ".x-audio", function(e) {
         var src = $(this).attr("data-src");
         layer.open({
             type: 2,
