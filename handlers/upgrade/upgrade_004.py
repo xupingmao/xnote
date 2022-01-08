2c15e590e14135b8fdb5156cc0069eb955123aa3
xupingmao <578749341@qq.com>
2022-01-08 12:02:36 +0800

    Update upgrade_004.py

--- handlers/upgrade/upgrade_004.py
+++ handlers/upgrade/upgrade_004.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # @author xupingmao
 # @since 2022/01/08 11:04:03
-# @modified 2022/01/08 11:07:21
+# @modified 2022/01/08 12:00:44
 # @filename upgrade_004.py
 
 """note_public索引重建"""
@@ -14,13 +14,16 @@ from handlers.upgrade.upgrade_main import is_upgrade_done
 from handlers.upgrade.upgrade_main import mark_upgrade_done
 
 def do_upgrade():
-    if is_upgrade_done("upgrade_004"):
+    if is_upgrade_done("upgrade_004.2"):
         log_info("upgrade_004 done")
         return
 
     db = dbutil.get_table("note_public")
     for value in db.iter(limit = -1):
+        if value.share_time is None:
+            value.share_time = value.ctime
+            db.update(value)
         db.rebuild_index(value, value.creator)
 
-    mark_upgrade_done("upgrade_004")
+    mark_upgrade_done("upgrade_004.2")
 
