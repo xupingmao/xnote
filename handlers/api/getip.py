0dc7f9d47a7e30c40d8ab5257b781e7c924c0ef3
xupingmao <xupingmao@gmail.com>
2023-03-04 23:20:42 +0800

    Update getip.py

--- handlers/api/getip.py
+++ handlers/api/getip.py
@@ -4,6 +4,13 @@
 # @modified 2019/07/12 00:52:34
 import web
 import xutils
+import xtemplate
+
+def get_real_ip():
+    real_ip_list = web.ctx.env.get("HTTP_X_FORWARDED_FOR")
+    if real_ip_list != None and len(real_ip_list) > 0:
+        return real_ip_list.split(",")[0]
+    return web.ctx.env.get("REMOTE_ADDR")
 
 class handler:
 
@@ -18,3 +25,21 @@ class handler:
         yield "\nRemote Addr: "
         yield web.ctx.env.get("REMOTE_ADDR")
         yield "\n"
+
+class JsonpHandler:
+
+    tpl = """
+{{callback}}({
+    real_ip: "{{real_ip}}"
+});
+    """
+
+    def GET(self):
+        callback = xutils.get_argument_str("callback", "callback")
+        real_ip = get_real_ip()
+        return xtemplate.render_text(self.tpl, real_ip = real_ip, callback = callback)
+
+xurls = (
+    r"/api/getip", handler,
+    r"/api/getip.jsonp", JsonpHandler,
+)
