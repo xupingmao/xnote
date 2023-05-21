f662df9afb719c6ad09ad608268d6fc526d1f7f4
xupingmao <xupingmao@gmail.com>
2023-05-22 00:20:55 +0800

    启动严格校验

--- core/xnote_migrate/upgrade_012.py
+++ core/xnote_migrate/upgrade_012.py
@@ -4,7 +4,7 @@
 @email        : 578749341@qq.com
 @Date         : 2023-05-20 22:54:35
 @LastEditors  : xupingmao
-@LastEditTime : 2023-05-20 23:28:07
+@LastEditTime : 2023-05-22 00:04:16
 @FilePath     : /xnote/core/xnote_migrate/upgrade_012.py
 @Description  : 描述
 """
@@ -26,16 +26,15 @@ def migrate_user_20230520():
         user_info.name = item.name
         user_info.password = item.password
         user_info.salt = item.salt
-        user_info.ctime = item.ctime
-        user_info.mtime = item.mtime
-        user_info.token = item.token
-        user_info.login_time = item.login_time
 
-        if user_info.mtime == None:
-            user_info.mtime = dateutil.format_datetime()
+        if item.login_time != None:
+            user_info.login_time = item.login_time
+
+        if item.mtime != None:
+            user_info.mtime = item.mtime
         
-        if user_info.ctime == None:
-            user_info.ctime = user_info.mtime
+        if item.ctime != None:
+            user_info.ctime = item.mtime
 
         record = xauth.UserModel.get_by_name(item.name)
         if record == None:
