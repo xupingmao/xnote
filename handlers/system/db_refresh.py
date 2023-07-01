9eb655e76873ec2cbae6a0727fbdf73b6f2170fc
xupingmao <xupingmao@gmail.com>
2023-07-01 18:44:01 +0800

    Update db_refresh.py

--- handlers/system/db_refresh.py
+++ handlers/system/db_refresh.py
@@ -4,7 +4,7 @@
 @email        : 578749341@qq.com
 @Date         : 2022-05-03 22:43:20
 @LastEditors  : xupingmao
-@LastEditTime : 2023-06-22 09:25:39
+@LastEditTime : 2023-07-01 18:29:41
 @FilePath     : /xnote/handlers/system/db_refresh.py
 @Description  : 数据库定时任务
 """
@@ -13,6 +13,7 @@ import xauth
 from xutils import dbutil
 from xutils import cacheutil
 from xutils.db import dbutil_cache
+from xutils.db.binlog import BinLog
 
 class RefreshHandler:
 
@@ -27,6 +28,8 @@ class RefreshHandler:
         db_cache.clear_expired()
 
         cacheutil._global_cache.clear_expired()
+        BinLog.get_instance().delete_expired()
+        
         return result
 
 
