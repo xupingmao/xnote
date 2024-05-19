f80b66cf5fd6ca262b58eb4de201e41d267b397c
xupingmao <xupingmao@gmail.com>
2024-05-19 17:21:10 +0800

    文件详情展示用户和用户组

--- handlers/fs/fs_api.py
+++ handlers/fs/fs_api.py
@@ -4,7 +4,7 @@
 @email        : 578749341@qq.com
 @Date         : 2022-05-22 00:17:06
 @LastEditors  : xupingmao
-@LastEditTime : 2024-05-03 13:35:45
+@LastEditTime : 2024-05-19 17:20:25
 @FilePath     : /xnote/handlers/fs/fs_api.py
 @Description  : 文件API
 """
@@ -34,6 +34,26 @@ class FileConfigHandler:
 
 class FileDetailHandler:
 
+    def get_user_name_by_uid(self, uid=0):
+        try:
+            import pwd
+            user_info = pwd.getpwuid(uid)
+            if user_info != None:
+                return user_info.pw_name
+        except:
+            pass
+        return f"uid({uid})"
+        
+    def get_group_name_by_gid(self, gid=0):
+        try:
+            import grp
+            group_info = grp.getgrgid(gid)
+            if group_info != None:
+                return group_info.gr_name
+        except:
+            pass
+        return f"gid({gid})"
+
     @xauth.login_required("admin")
     def GET(self):
         fpath = xutils.get_argument_str("fpath")
@@ -48,6 +68,8 @@ class FileDetailHandler:
             detail_msg += f"\n变更时间: {dateutil.format_datetime(stat.st_ctime)}"
             detail_msg += f"\n访问时间: {dateutil.format_datetime(stat.st_atime)}"
             detail_msg += f"\n文件大小: {fsutil.format_size(stat.st_size)}"
+            detail_msg += f"\n用户: {self.get_user_name_by_uid(stat.st_uid)}"
+            detail_msg += f"\n用户组: {self.get_group_name_by_gid(stat.st_gid)}"
             return webutil.SuccessResult(data=detail_msg)
         except:
             xutils.print_exc()
