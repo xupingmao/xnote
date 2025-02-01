d370402599c86a2edfbd57361c5512b0b83576f6
xupingmao <xupingmao@gmail.com>
2025-02-01 15:20:31 +0800

    删除废弃代码

--- handlers/tools/tools.py
+++ handlers/tools/tools.py
@@ -1,10 +1,10 @@
 # encoding=utf-8
 import os
-import web
 import xutils
 
 from xnote.core import xtemplate
 from xnote.core import xconfig
+from xnote.core import xauth
 
 C_TEMPLATE = """
 #include <stdio.h>
@@ -18,19 +18,20 @@ int main(int argc, char* argv) {
 """
 
 class TccHandler:
+
+    @xauth.login_required("admin")
     def GET(self):
-        args = web.input(code=None, json=None)
-        code = args.code
-        json = args.json
+        code = xutils.get_argument_str("code")
+        return_json = xutils.get_argument_bool("json")
         output = ""
-        if code is None:
+        if code is None or code == "":
             code = C_TEMPLATE
         else:
             path = os.path.join(xconfig.TMP_DIR, "temp.c")
             xutils.savetofile(path, code)
             status, output = xutils.getstatusoutput("D:\\tcc\\tcc.exe -run %s" % path)
 
-            if json == "true":
+            if return_json:
                 return xutils.json_str(status=status, output=output)
         return xtemplate.render("tools/tcc.html", 
             show_aside = False,
--- tools/upload-to-pip.py
+++ tools/upload-to-pip.py
@@ -12,8 +12,14 @@ def main():
     if os.path.exists("xnote_web.egg-info"):
         shutil.rmtree("xnote_web.egg-info")
 
-    os.system(f"{sys.executable} setup.py")
-    os.system(f"{sys.executable} -m twine upload --username __token__ --paasword {pypi_token}")
+    dist_dir = "dist"
+    for fname in os.listdir(dist_dir):
+        fpath = os.path.join(dist_dir, fname)
+        print(f"removing {fpath}")
+        os.remove(fpath)
+
+    os.system(f"{sys.executable} setup.py sdist")
+    os.system(f"{sys.executable} -m twine upload --username __token__ --password {pypi_token} dist/*")
 
 if __name__ == "__main__":
     fire.Fire(main)
